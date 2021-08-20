e30e20c035e2d3b914eba0dd046aabade74c976c
jxxjxxlxx <92627.p@gmail.com>
2021-08-20 09:59:41 +0200

    Reorganized folders

---
+++ client/src/pages/Create/CreatePlzHelp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePlzHelp = () => {
+	return (
+		<div>
+			
+		</div>
+	)
+}
+
+export default CreatePlzHelp
---
+++ client/src/pages/MainPages/PlzHelp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavMain from "../components/NavMain/NavMain";
+
+const PlzHelp = () => {
+	return (
+		<div>
+			<NavMain>
+			</NavMain>
+			<h1> Plzz help !! </h1>
+		</div>
+	)
+}
+
+export default PlzHelp
