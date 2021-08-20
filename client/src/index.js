c5b37416527ea9b41a2c6e67f680b9540752e0a2
jxxjxxlxx <92627.p@gmail.com>
2021-08-20 16:01:29 +0200

    comment routes done in comment.js and app.js, fixed error on comment schema model, all seems working so far

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserProvider from './pages/Auth/UserProvider';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <UserProvider>
     <App />
+    </UserProvider>
     </BrowserRouter>
-  
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ client/src/pages/Create/FormDILG.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+// import axios from 'axios'
+
+// const FormDILG = () => {
+//     return (
+//         <div>
+//             <p1>Show me your OOTD</p1>
+//             <form>
+//                 <div>
+//                     <label htmlFor="occasion">Occasion of outfit</label>
+//                     <input type="text" id="occasionOfOutfit" name="occasionOfOutfit" value=""
+//                 </div>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default FormDILG
