538db34f7bce87e47b6a08cce23588f016baff05
jxxjxxlxx <92627.p@gmail.com>
2021-08-20 09:07:13 +0200

    app.js in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Landing from './pages/Landing';
+import Home from "./pages/Home";
+import UserProfile from "./pages/UserProfile";
+import DILG from "./pages/DILG";
+import CreateDILG from "./pages/CreateDILG";
+import PlzHelp from "./pages/PlzHelp"
+import CreatePlzHelp from "./pages/CreatePlzHelp";
+import EditDILG from "./pages/EditDILG";
+import EditPlzHelp from "./pages/EditPlzHelp";
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+
+        <Route exact path="/" component={Landing}/>
+        <Route exact path="/home" component={Home}/>
+
+        <Route exact path="/ilookgood" component={DILG}/>
+        <Route exact path="/plzhelp" component={PlzHelp}/>
+
+        <Route exact path="/ilookgood/post" component={CreateDILG}/>
+        <Route exact path="/plzhelp/post" component={CreatePlzHelp}/>
+
+        <Route exact path="/ilookgood/:id/edit" component={EditDILG}/>
+        <Route exact path="/plzhelp/:id/edit" component={EditPlzHelp}/>
+
+        <Route exact path="/signin" component={SignIn}/>
+        <Route exact path="/signup" component={SignUp}/>
+
+        <Route exact path="UserProfile" component={UserProfile}/>
+        
+      </Switch>
     </div>
   );
 }
