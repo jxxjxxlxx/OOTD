277e4b5d200c27db7114558b67c74f33e6e431e8
jxxjxxlxx <92627.p@gmail.com>
2021-08-24 22:11:36 +0200

    detail page done without displaying the postedComment

--- client/src/pages/OutfitDetails/DILGOutfitDetail.jsx
+++ client/src/pages/OutfitDetails/DILGOutfitDetail.jsx
@@ -1,16 +1,54 @@
 import React from 'react';
-import axios from 'axios'
 import Comment from '../../components/Comment/Comment'
+import apiHandler from '../../api/apiHandler'
+// import axios from 'axios'
+const {service} = apiHandler
 
-const OutfitDetail = () => {
-	return (
+class OutfitDetail extends React.Component {
+	
+	state = {
+		postingUser:"",
+		image:"",
+		itemDescription:"",
+		occasionOfOutfit:"",
+		outfitMoodCommnet:"",
+		postingTime:"",
+	};
+
+	componentDidMount () {
+		const id = this.props.match.params.id;
+
+		service 
+		.get(`http://localhost:7777/api/ilookgood/${id}`)
+		
+		.then((apiRes)=>{
+			const post = apiRes.data
+			this.setState({
+				...post,
+			})
+		})
+		.catch((error)=>{console.log(error)})
+	}
+	
+	render() {
+		
+		return (
+			
 		<div>
 			<h1> Outfit detail </h1>
+			<div key={this.state.postId}>
+			<p>{this.state.postingUser}</p>
+			<img src={this.state.image} alt="post picture"/>
+			<p>{this.state.itemDescription}</p>
+			<p>{this.state.occasionOfOutfit}</p>
+			<p>{this.state.outfitMoodCommnet}</p>
+			<p>{this.state.postingTime}</p>
+			</div>
 
-		
 			<Comment/>
 		</div>
-	)
+	)}
+
 }
 
 export default OutfitDetail
