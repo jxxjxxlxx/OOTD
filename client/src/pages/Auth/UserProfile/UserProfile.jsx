1c089cb8c52df2d72dd960353d7ef7644e8d7450
jxxjxxlxx <92627.p@gmail.com>
2021-08-26 15:33:15 +0200

    css start

--- client/src/pages/Auth/UserProfile/UserProfile.jsx
+++ client/src/pages/Auth/UserProfile/UserProfile.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import NavMain from "../../../components/NavMain/NavMain";
-<<<<<<< HEAD
-import FeedBack from "../../../base/FeedBack";
-import { withUser } from "../../Auth/withUser";
-=======
-import withUser from "../withUser";
->>>>>>> d29b43bc0791ee8aee9fb4b375576e95561c042a
+import {withUser} from "../withUser";
 import  apiHandler  from '../../../api/apiHandler';
 import { Link } from "react-router-dom";
 
@@ -21,31 +16,9 @@ class UserProfile extends React.Component {
 
 	};
 
-<<<<<<< HEAD
-
-
-
-
-	compounenttDidMount () {
-
-	service
-
-	
-      	.get("/api/me/posts")
-	     
-      	.then((apiResponse) => {
-        	this.setState({ coolPosts: apiResponse.data });
-		console.log(this.state.apiResponse.data)
-      		})
-		
-      	.catch((error) => {
-		     
-        	(console.log(error));
-=======
 	componentDidMount () {
 	
 	service
-
 	
 	.get("/api/users/me/posts")
 	.then((apiResponse) => {
@@ -54,7 +27,6 @@ class UserProfile extends React.Component {
 		
       	.catch((error) => {
         	console.log(error)
->>>>>>> d29b43bc0791ee8aee9fb4b375576e95561c042a
       	});
 	}
 	 
@@ -70,24 +42,16 @@ class UserProfile extends React.Component {
 				
 				return(
 					<div key={this.state.posts}>
-<<<<<<< HEAD
-					
-					
-					<ul>
-						
-					<li> <img src={posts.image} alt="pic here" ></img></li>
-					</ul>
-=======
 
 					<Link to={`/ilookgood/${posts._id}/edit`}>
 					<img src={posts.image} alt="pic here" />
 					</Link>
->>>>>>> d29b43bc0791ee8aee9fb4b375576e95561c042a
 					</div>
 				)
 			})}
 		</div>
 		)
 	}
-};
-export default withUser(UserProfile);;
+}
+
+export default withUser(UserProfile);
--- client/src/pages/MainPages/DILG.jsx
+++ client/src/pages/MainPages/DILG.jsx
@@ -1,62 +1,64 @@
-import React from 'react';
+import React from "react";
 import NavMain from "../../components/NavMain/NavMain";
 import { NavLink } from "react-router-dom";
-import {Link} from "react-router-dom";
-import axios from 'axios';
-import './DILG.css';
-import apiHandler from '../../api/apiHandler';
+import { Link } from "react-router-dom";
+import "./DILG.css";
+import apiHandler from "../../api/apiHandler";
 
 const { service } = apiHandler;
 
 class DILG extends React.Component {
+  state = {
+    coolPosts: [],
+  };
 
-		state= {
-			coolPosts: [],
-		};
-	
-	componentDidMount() {
-		service
-		.get("api/ilookgood")
-		.then((apiRes)=>{
-			this.setState({
-				coolPosts: apiRes.data,
-			})
-			console.log(apiRes)
-		})
-		.catch((error)=>{console.log(error)})
-	}	
-	render() {
-	return (
-		<div>
-			<NavMain></NavMain>
-			<div className="DILG">
-			
-			<h1> DILG </h1>
-			</div>
-			
-			<div>
-			<NavLink to="/ilookgood/post" className="posthere"> post here </NavLink>
-			</div>
-			{this.state.coolPosts.map((coolPost)=>{
-				return(
+  componentDidMount() {
+    service
+      .get("api/ilookgood")
+      .then((apiRes) => {
+        this.setState({
+          coolPosts: apiRes.data,
+        });
+        console.log(apiRes);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <>
+        <NavMain/>
+        <div className="mainPageHeader">
+          <h1> DILG </h1>
+        </div>
 
-					<div className="grid-container">
-					<div className="coolpost-grid">
-					<div key={coolPost._id}>
-						<Link to={`/ilookgood/${coolPost._id}`}>
-							<img src={coolPost.image} alt={coolPost.postingUser.userName}/>
-						</Link>
-					
-					<p>{coolPost.postingUser.userName}</p>
-					</div>
-					</div>
-					</div>
-				)
-			})}
-		</div>
-	)
+		<div className="postHereBtn">
+          <NavLink to="/ilookgood/post" className="posthere">
+            post here
+          </NavLink>
+        </div>
+
+        {this.state.coolPosts.map((coolPost) => {
+          return (
+            <div className="mainContainer">
+              <div className="mainPost" key={coolPost._id}>
+                <Link to={`/ilookgood/${coolPost._id}`}>
+                  <img
+                    className="postImg"
+                    src={coolPost.image}
+                    alt={coolPost.postingUser.userName}
+                  />
+                </Link>
+
+                <p className="postUser">{coolPost.postingUser.userName}</p>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
+  }
 }
-}
 
-
-export default DILG
+export default DILG;
